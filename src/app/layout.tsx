d6949b352c85b6466a9cff33d2afbe0f7af7a04b
Rido A <98310463+ridoamali@users.noreply.github.com>
2025-03-06 12:59:19 +0700

    revision on google font nunito 600 v6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from 'next/font/google'
 import "./globals.css";
 import FontAwesomeConfig from "./fontawesome";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const nunito = Nunito({
+  weight: '600',
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -29,7 +27,7 @@ export default function RootLayout({
         <FontAwesomeConfig />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={nunito.className}
       >
         {children}
       </body>
