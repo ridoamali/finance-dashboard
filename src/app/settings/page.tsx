67caac1a278a447d4b90af23f652e8f1a914f342
Rido A <98310463+ridoamali@users.noreply.github.com>
2025-03-06 12:37:16 +0700

    revision pages link v6

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+'use client'
+
+
+import Link from 'next/link';
+import React from 'react';
 
 const page = () => {
   return (
     <>
     <h1 className='text-center text-4xl'>Ini Halaman Settings</h1>
     <div className='text-center'>
-        <a href="/">HOME</a>
+        <button className='w-20 h-10 border-1 bg-purple-100 rounded-full'>
+          <Link href={'/'} >Home</Link>
+        </button>
     </div>
     </>
   )
